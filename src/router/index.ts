690f45e6b5e235eea1dffc4ab778e8cc3ae8b80f
cwj <595842570@qq.com>
2023-05-04 21:26:40 +0800

    feat: 引入bowser

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import NProgress from 'nprogress';
+import Bowser from 'bowser';
 import 'nprogress/nprogress.css'; //
 import routes from './routes';
 
 import { trackWeb } from '@/apis/user';
 import { getTime } from '@/utils/comFunc';
-import { browserType } from '@/utils/comFunc';
 
 const router = createRouter({
   history: createWebHistory(), //可传参数，配置base路径，例如'/app'
@@ -13,6 +13,8 @@ const router = createRouter({
 });
 
 NProgress.configure({ showSpinner: false });
+
+const bowser = Bowser.getParser(window.navigator.userAgent).getBrowserName();
 
 router.beforeEach((to, from) => {
   NProgress.start();
@@ -32,7 +34,7 @@ router.beforeEach((to, from) => {
   trackWeb({
     data: JSON.stringify({
       vs: 'vue3-manage-vite',
-      deviceType: browserType(),
+      deviceType: bowser,
       url: to.path,
       referer: from.path,
       localTime: getTime(),
--- src/utils/comFunc.ts
+++ src/utils/comFunc.ts
@@ -27,41 +27,6 @@ export function getTime() {
   return time;
 }
 
-//判断浏览器类型
-export function browserType() {
-  let userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
-  //判断是否Opera浏览器
-  let isOpera = userAgent.indexOf('Opera') > -1;
-  //判断是否IE浏览器
-  let isIE = userAgent.indexOf('compatible') > -1 && userAgent.indexOf('MSIE') > -1 && !isOpera;
-  //判断是否IE的Edge浏览器
-  let isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf('rv:11.0') > -1;
-  let isEdge = userAgent.indexOf('Edge') > -1 && !isIE;
-  //判断是否Firefox浏览器
-  let isFF = userAgent.indexOf('Firefox') > -1;
-  //判断是否Safari浏览器
-  let isSafari = userAgent.indexOf('Safari') > -1 && userAgent.indexOf('Chrome') === -1;
-  //判断Chrome浏览器
-  let isChrome = userAgent.indexOf('Chrome') > -1 && userAgent.indexOf('Safari') > -1;
-
-  if (isIE) {
-    let reIE = new RegExp('MSIE (\\d+\\.\\d+);');
-    reIE.test(userAgent);
-    let fIEVersion = parseFloat(RegExp['$1']);
-    if (fIEVersion === 7) return 'IE7';
-    else if (fIEVersion === 8) return 'IE8';
-    else if (fIEVersion === 9) return 'IE9';
-    else if (fIEVersion === 10) return 'IE10';
-    else return 'IE7以下'; //IE版本过低
-  }
-  if (isIE11) return 'IE11';
-  if (isEdge) return 'Edge';
-  if (isFF) return 'FF';
-  if (isOpera) return 'Opera';
-  if (isSafari) return 'Safari';
-  if (isChrome) return 'Chrome';
-}
-
 //引入静态资源
 export const getAssetsFile = (url: string) => {
   return new URL(`../assets/images/${url}`, import.meta.url).href;
