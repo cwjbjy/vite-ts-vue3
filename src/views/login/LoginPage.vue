3f3ef6ef62cfbf0991169de88ac0be0e66bb227a
cwjbjy <595842570@qq.com>
2023-12-04 20:57:02 +0800

    feat: 增加流程图

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -2,7 +2,11 @@
   <div v-title="title" class="login">
     <header class="top">
       <div class="title">PC端管理系统(Vue版)</div>
-      <div class="subTitle">已通过Docker+Jenkins+Webhooks实现自动化打包+部署+邮件通知</div>
+      <div class="subTitle">
+        已通过Docker+Jenkins+Webhooks实现自动化打包+部署+邮件通知<span class="flow" @click="dialogModal = true">
+          （流程图）
+        </span>
+      </div>
     </header>
     <main class="main">
       <article class="form">
@@ -24,11 +28,47 @@
       <br />
       <span class="link" @click="thirdParty">苏ICP备20022574号-2</span>
     </footer>
+    <el-dialog v-model="dialogModal" title="流程图" width="90%">
+      <div>1. 通过Git提交代码到GitHub</div>
+      <div>2. GitHub通过Webhooks通知Jenkins</div>
+      <div>3. Jenkins fetch源码到工作空间</div>
+      <div>4. 编译下载到工作空间的源码</div>
+      <div>5. 对源码进行打包</div>
+      <div>6. 将打包后的代码发送到云服务器指定目录</div>
+      <div>7. 邮件通知</div>
+      <div class="pic">
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <div class="gutter-row">
+              1. <img :src="getAssetsFile('login/1.png')" class="headPortrait" alt="加载失败" /></div
+          ></el-col>
+          <el-col :span="12">
+            <div class="gutter-row">
+              1. <img :src="getAssetsFile('login/2.png')" class="headPortrait" alt="加载失败" />
+            </div>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <div class="gutter-row">
+              3. <img :src="getAssetsFile('login/3.png')" class="headPortrait" alt="加载失败" />
+            </div>
+          </el-col>
+          <el-col :span="12">
+            <div class="gutter-row">
+              4. <img :src="getAssetsFile('login/4.png')" class="headPortrait" alt="加载失败" />
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onBeforeUnmount, onMounted, ref } from 'vue';
+
+import { getAssetsFile } from '@/utils/share';
 
 import LoginForm from './components/LoginForm.vue';
 import LoginOther from './components/LoginOther.vue';
@@ -38,6 +78,7 @@ import { GITHUB, BEI_AN } from '@/settings/user';
 
 const flag = ref(true);
 const title = ref('登录');
+const dialogModal = ref(false);
 const registerRef = ref<InstanceType<typeof LoginRegister> | null>(null);
 const loginRef = ref<InstanceType<typeof LoginForm> | null>(null);
 
@@ -75,6 +116,17 @@ onBeforeUnmount(() => {
   min-width: 600px;
   color: $color-font;
   background-color: $background-login;
+  .pic {
+    margin-top: 10px;
+  }
+  .gutter-row {
+    display: flex;
+  }
+  img {
+    object-fit: cover;
+    width: 400px;
+    margin-left: 10px;
+  }
   .top {
     width: 100%;
     height: 20vh;
@@ -88,6 +140,9 @@ onBeforeUnmount(() => {
     }
     .subTitle {
       padding-top: 6px;
+      .flow {
+        cursor: pointer;
+      }
     }
   }
   .main {
